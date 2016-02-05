f647a1791c875941e2c368c2e48ee6133f11d5ec
njruiz <njruiz.nr@gmail.com>
2016-02-05 09:50:24 +0800

    Initial

---
+++ CertificateTextEntityClassificationSystem/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace CertificateTextEntityClassificationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_presenter = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_date = new System.Windows.Forms.TextBox();
+            this.txt_recipient = new System.Windows.Forms.TextBox();
+            this.txt_award = new System.Windows.Forms.TextBox();
+            this.txt_type = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_ShowDB = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_terminate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rich_status = new System.Windows.Forms.RichTextBox();
+            this.txt_confidence = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(609, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "-OUTPUT-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(533, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Presenter:";
+            // 
+            // txt_presenter
+            // 
+            this.txt_presenter.Location = new System.Drawing.Point(536, 231);
+            this.txt_presenter.Name = "txt_presenter";
+            this.txt_presenter.Size = new System.Drawing.Size(224, 20);
+            this.txt_presenter.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(533, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(533, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Recipient:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(533, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Award:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(533, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Certificate Type:";
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(536, 280);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(224, 20);
+            this.txt_date.TabIndex = 23;
+            // 
+            // txt_recipient
+            // 
+            this.txt_recipient.Location = new System.Drawing.Point(536, 179);
+            this.txt_recipient.Name = "txt_recipient";
+            this.txt_recipient.Size = new System.Drawing.Size(224, 20);
+            this.txt_recipient.TabIndex = 22;
+            // 
+            // txt_award
+            // 
+            this.txt_award.Location = new System.Drawing.Point(536, 127);
+            this.txt_award.Name = "txt_award";
+            this.txt_award.Size = new System.Drawing.Size(224, 20);
+            this.txt_award.TabIndex = 21;
+            // 
+            // txt_type
+            // 
+            this.txt_type.Location = new System.Drawing.Point(536, 79);
+            this.txt_type.Name = "txt_type";
+            this.txt_type.Size = new System.Drawing.Size(224, 20);
+            this.txt_type.TabIndex = 20;
+            // 
+            // btn_start
+            // 
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.Location = new System.Drawing.Point(601, 312);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(107, 37);
+            this.btn_start.TabIndex = 19;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_ShowDB
+            // 
+            this.btn_ShowDB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_ShowDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ShowDB.Location = new System.Drawing.Point(534, 443);
+            this.btn_ShowDB.Name = "btn_ShowDB";
+            this.btn_ShowDB.Size = new System.Drawing.Size(82, 25);
+            this.btn_ShowDB.TabIndex = 34;
+            this.btn_ShowDB.Text = "Show DB";
+            this.btn_ShowDB.UseVisualStyleBackColor = true;
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pause.Location = new System.Drawing.Point(534, 408);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(82, 25);
+            this.btn_pause.TabIndex = 33;
+            this.btn_pause.Text = "Pause";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_terminate
+            // 
+            this.btn_terminate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_terminate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_terminate.Location = new System.Drawing.Point(534, 479);
+            this.btn_terminate.Name = "btn_terminate";
+            this.btn_terminate.Size = new System.Drawing.Size(82, 25);
+            this.btn_terminate.TabIndex = 32;
+            this.btn_terminate.Text = "Terminate";
+            this.btn_terminate.UseVisualStyleBackColor = true;
+            this.btn_terminate.Click += new System.EventHandler(this.btn_terminate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Status:";
+            // 
+            // rich_status
+            // 
+            this.rich_status.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.rich_status.Location = new System.Drawing.Point(12, 408);
+            this.rich_status.Name = "rich_status";
+            this.rich_status.ReadOnly = true;
+            this.rich_status.Size = new System.Drawing.Size(500, 153);
+            this.rich_status.TabIndex = 30;
+            this.rich_status.Text = "";
+            // 
+            // txt_confidence
+            // 
+            this.txt_confidence.Location = new System.Drawing.Point(534, 525);
+            this.txt_confidence.Name = "txt_confidence";
+            this.txt_confidence.ReadOnly = true;
+            this.txt_confidence.Size = new System.Drawing.Size(82, 20);
+            this.txt_confidence.TabIndex = 35;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(533, 548);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Confidence (%)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 573);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_confidence);
+            this.Controls.Add(this.btn_ShowDB);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_terminate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rich_status);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_presenter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.txt_recipient);
+            this.Controls.Add(this.txt_award);
+            this.Controls.Add(this.txt_type);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_presenter;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_date;
+        private System.Windows.Forms.TextBox txt_recipient;
+        private System.Windows.Forms.TextBox txt_award;
+        private System.Windows.Forms.TextBox txt_type;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_ShowDB;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_terminate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rich_status;
+        private System.Windows.Forms.TextBox txt_confidence;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CertificateTextEntityClassificationSystem/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using Tesseract;
+using System.IO;
+using edu.stanford.nlp.ie.crf;
+
+namespace CertificateTextEntityClassificationSystem
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap image;
+        private int count;
+        private double totalConfidence;
+        private string output;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            rich_status.Text += "Removing Image Noise...\n";
+            image = new Bitmap(pictureBox1.Image);
+            preprocess(image);
+            //ConservativeSmoothing filter = new ConservativeSmoothing();
+            GaussianSharpen filter = new GaussianSharpen(4, 11);
+            filter.ApplyInPlace(image);
+
+            rich_status.Text += "Image Smoothening Successful.\n";
+            //image = greyscaleFilter.Apply(image);
+            //rich_status.Text += "Image Greyscaled\n";
+            pictureBox1.Image = image;
+
+            //engine = new TesseractEngine(@"D:\PERSONAL\NJ (LT)\THESIS IT\Project\CertificateTextEntityClassificationSystem\CertificateTextEntityClassificationSystem\bin\Debug\tessdata", "eng", EngineMode.Default);
+            //engine.DefaultPageSegMode = PageSegMode.SingleBlock;
+
+            try
+            {
+                tessnet2.Tesseract ocr = new tessnet2.Tesseract();
+                ocr.SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,$-/#&=()\"':?");
+                ocr.Init(@"D:\PERSONAL\NJ (LT)\THESIS IT\Project\CertificateTextEntityClassificationSystem\CertificateTextEntityClassificationSystem\bin\x64\Debug\tessdata", "eng", false);
+                List<tessnet2.Word> result = ocr.DoOCR(image, Rectangle.Empty);
+                //var page = engine.Process(image);
+                foreach (tessnet2.Word word in result)
+                {
+                    count++;
+                    totalConfidence += word.Confidence;
+                    //rich_status.Text += word.Confidence + ", " + word.Text + ", " + word.Top + ", " + word.Bottom + ", " + word.Left + ", " + word.Right + "\n";
+                    //rich_status.Text += ("" + word.Text + "(" + word.Confidence + ")\n");
+                    output = string.Join(" ", result.Select(x => x.Text).ToList());
+                    rich_status.Text = output;
+                    using (StreamWriter writetext = new StreamWriter(@"D:\PERSONAL\NJ (LT)\THESIS IT\Project\CertificateTextEntityClassificationSystem\Results.txt"))
+                    {
+                        writetext.WriteLine(output);
+                    }
+                }
+                //recognize(output);
+                totalConfidence = (totalConfidence / count);
+                txt_confidence.Text = totalConfidence.ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            if (btn_pause.Text == "Pause")
+            {
+                btn_pause.Text = "Resume";
+                rich_status.Text += "Process Paused\n";
+            }
+            else
+            {
+                btn_pause.Text = "Pause";
+                rich_status.Text += "Process Resumed\n";
+            }
+        }
+
+        private void btn_terminate_Click(object sender, EventArgs e)
+        {
+            rich_status.Text += "Application is shutting down...\n";
+
+            DialogResult d = MessageBox.Show("Are you sure you want this?",
+                                             "Application is shutting down.",
+                                             MessageBoxButtons.YesNo,
+                                             MessageBoxIcon.Question);
+            if (d != DialogResult.No)
+            {
+                Close();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            pictureBox1.ImageLocation = file.FileName;
+            rich_status.Text = "File Opened: " + Convert.ToString(file.FileName) + "\n";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void preprocess(Bitmap image)
+        {
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                var gray_matrix = new float[][]
+                {
+                    new float[] { 0.299f, 0.299f, 0.299f, 0, 0 },
+                    new float[] { 0.587f, 0.587f, 0.587f, 0, 0 },
+                    new float[] { 0.114f, 0.114f, 0.114f, 0, 0 },
+                    new float[] { 0,      0,      0,      1, 0 },
+                    new float[] { 0,      0,      0,      0, 1 }
+                };
+
+                var imageAttrib = new System.Drawing.Imaging.ImageAttributes();
+                imageAttrib.SetColorMatrix(new System.Drawing.Imaging.ColorMatrix(gray_matrix));
+                imageAttrib.SetThreshold((float)0.7);
+                var rec = new Rectangle(0, 0, image.Width, image.Height);
+                g.DrawImage(image, rec, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imageAttrib);
+            }
+
+        }
+
+        private void recognize(String data)
+        {
+            //public static CRFClassifier classifier = CRFClassifier.getClassifierNoExceptions()
+            var root = @"..\..\..\..\paket-files\nlp.stanford.edu\stanford-ner-2015-12-09";
+            var directory = root + @"\classifiers";
+            var classifier = CRFClassifier.getClassifierNoExceptions(directory + @"\english.all.3class.distsim.crf.ser.gz");
+            rich_status.Text += classifier.classifyToString(data);
+        }
+    }
+}
