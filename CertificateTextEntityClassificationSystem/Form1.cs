161bbb53b142d6835fe3428496561843abb0935f
njruiz <njruiz.nr@gmail.com>
2016-02-15 20:33:36 +0800

    Date and Certificate Type
    -Used Regex

--- CertificateTextEntityClassificationSystem/Form1.cs
+++ CertificateTextEntityClassificationSystem/Form1.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Imaging.Filters;
 using System.IO;
 using edu.stanford.nlp.ie.crf;
 using edu.stanford.nlp.util;
+using Tesseract;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CertificateTextEntityClassificationSystem
 {
     public partial class Form1 : Form
     {
         private Bitmap image;
-        private int count;
         private double totalConfidence;
         private string output;
+        private string date;
+        private string certificateType;
+        private int count;
         string wanted_path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
         string path = System.IO.Directory.GetCurrentDirectory();
         public Form1()
@@ -31,24 +31,36 @@ namespace CertificateTextEntityClassificationSystem
         {
             try
             {
-                tessnet2.Tesseract ocr = new tessnet2.Tesseract();
-                ocr.SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,$-/#&=()\"'");
-                ocr.Init(path + @"\tessdata", "eng", false);
-                List<tessnet2.Word> result = ocr.DoOCR(image, Rectangle.Empty);
-                foreach (tessnet2.Word word in result)
-                {
-                    count++;
-                    totalConfidence += word.Confidence;
-                    output = string.Join(" ", result.Select(x => x.Text).ToList());
-                    rich_status.Text = output;
-                    using (StreamWriter writetext = new StreamWriter(path + @"\Results.txt"))
-                    {
-                        writetext.WriteLine(output);
-                    }
-                }
-                rich_status.Text += recognize(output);
-                totalConfidence = (totalConfidence / count);
-                txt_confidence.Text = totalConfidence.ToString();
+                /*
+               var engine = new TesseractEngine(path, "eng", EngineMode.Default);
+               var page = engine.Process(image);
+               output = page.GetText();
+               rich_status.Text += output;
+               rich_status.Text += recognize(output);
+               rich_status.Text += "\nagi here" + checkHasDate(output) + getDate(output);
+               */
+
+               tessnet2.Tesseract ocr = new tessnet2.Tesseract();
+               ocr.SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,$-/#&=()'");
+               ocr.Init(path + @"\tessdata", "eng", false);
+               List<tessnet2.Word> result = ocr.DoOCR(image, Rectangle.Empty);
+               foreach (tessnet2.Word word in result)
+               {
+                   count++;
+                   totalConfidence += word.Confidence;
+                   output = string.Join(" ", result.Select(x => x.Text).ToList());
+                   rich_status.Text = output;
+                   using (StreamWriter writetext = new StreamWriter(path + @"\Results.txt"))
+                   {
+                       writetext.WriteLine(output);
+                   }
+               }
+               rich_status.Text += recognize(output);
+               txt_date.Text = getDate(output);
+               txt_type.Text = getCertificateType(output);
+               totalConfidence = (totalConfidence / count);               
+               txt_confidence.Text = totalConfidence.ToString();
+               
 
             }
             catch (Exception ex)
@@ -63,8 +75,8 @@ namespace CertificateTextEntityClassificationSystem
             image = new Bitmap(pictureBox1.Image);
             preprocess(image);
             //ConservativeSmoothing filter = new ConservativeSmoothing();
-            //GaussianSharpen filter = new GaussianSharpen(4, 11);
-            //filter.ApplyInPlace(image);
+            GaussianSharpen filter = new GaussianSharpen(4, 11);
+            filter.ApplyInPlace(image);
 
             rich_status.Text += "Image Pre-processing Successful.\n";
             pictureBox1.Image = image;
@@ -103,6 +115,12 @@ namespace CertificateTextEntityClassificationSystem
             OpenFileDialog file = new OpenFileDialog();
             file.ShowDialog();
             pictureBox1.ImageLocation = file.FileName;
+            txt_award.Text = "";
+            txt_confidence.Text = "";
+            txt_date.Text = "";
+            txt_presenter.Text = "";
+            txt_recipient.Text = "";
+            txt_type.Text = "";
             rich_status.Text = "File Opened: " + Convert.ToString(file.FileName) + "\n";
         }
 
@@ -149,9 +167,48 @@ namespace CertificateTextEntityClassificationSystem
         }
 
         private void btn_ShowDB_Click(object sender, EventArgs e)
+        {            
+            string temp = "|..|2i|i 2|r y an Cebu Educational Development Foundation for Information Technology atseaietintnrmat1auIerhnotogg|Philippine Information Technology General Certification Examination (Phil-IT GCE) Ruiz Nelson jr. Cebu Institute of Technology - University Examinee Name School This is to certify that you have successfully PASSED the Phil-IT General Certification Examination held on l December 26, 2015 at the Cebu Institute of Technology -- University , N. Buculso Avenue, Cebu City 6000 Philippines. y I e--e | It lvii | y it 'I OI-IN MIC L K. LAR|O WILFREDO T.| QR. | ,.,3 ,1.1 CEDF-IT yl Administrator CEDF-IT Manag g irector |L| gf";
+            //DateTime time = DateTime.Parse(temp);
+            rich_status.Text = checkHasDate(temp);
+            rich_status.Text += "agi here " + getDate(temp); // January 06, 2015
+        }
+
+        private String checkHasDate(string data)
         {
-            //output = "Nelson F. Ruiz Jr. December 11, 1995";
-            rich_status.Text = recognize(output);
+            string[] inputText = data.Split(' ');
+            bool hasDate = false;
+            DateTime dateTime = new DateTime();            
+            foreach(string text in inputText)
+            {                
+                try
+                {
+                    dateTime = DateTime.Parse(data);
+                    dateTime.ToString();
+                    hasDate = true;
+                    break;
+                }
+                catch (Exception ex) { }
+            }
+            return dateTime.ToString("MMMM");
+        }
+
+        private string getDate(string data)
+        {
+            string regex = @"(January|February|March|April|May|June|July|August|September|October|November|December) *(\d|[0-3]\d), *\d{4}";
+            string regex2 = @"(January|February|March|April|May|June|July|August|September|October|November|December) *(\d|[0-3]\d) . *\d{4}";
+            if(Regex.Match(data, regex) != null)
+                date = Regex.Match(data, regex).Value;
+            else if(Regex.Match(data, regex2) != null)
+                date = Regex.Match(data, regex2).Value;
+            return date;
+        }
+
+        private string getCertificateType(string data)
+        {
+            string regex = @"(Certificate of|Certificate Of|CERTIFICATE OF|certificate of|CERTIFICATE of) \w+";
+            certificateType = Regex.Match(data, regex).Value;
+            return certificateType;
         }
     }
 }
